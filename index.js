333da2fd4ec35ac878c3ae0572b74f23963571a4
Angela Castle <eyesofthegoddess@hotmail.com>
2026-07-18 17:49:16 -0700

    corrections to asset and spelling

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let contrastToggle = false;
 const scaleFactor = 1 / 20;
 
 function moveBackground(event) {
-  const shapes = document.querySelectorAll(".shape");
+  const shapes = document.querySelectorAll(`.shape`);
   const x = event.clientX * scaleFactor;
   const y = event.clientY * scaleFactor;
 
@@ -17,33 +17,33 @@ function moveBackground(event) {
 function toggleContrast() {
   contrastToggle = !contrastToggle;
   if (contrastToggle) {
-    document.body.classList += " dark-theme"
+    document.body.classList += ` dark-theme`
   }
   else {
-    document.body.classList.remove("dark-theme")
+    document.body.classList.remove(`dark-theme`)
   }
 }
 
 function contact(event) {
   event.preventDefault();
-  const loading = document.querySelector(".modal__overlay--loading");
-  const success = document.querySelector(".modal__overlay--success");
-  loading.classList += " modal__overlay--visible";
+  const loading = document.querySelector(`.modal__overlay--loading`);
+  const success = document.querySelector(`.modal__overlay--success`);
+  loading.classList += ` modal__overlay--visible`;
   emailjs
     .sendForm(
-      "template_iku491j",
-      "service_4zynz44"
+      `template_iku491j`,
+      `service_4zynz44`,
       event.target,
-      "user_yKXbOENcjMJ_G1was"
+      `user_yKXbOENcjMJ_G1was`
     )
     .then(() => {
-      loading.classList.remove("modal__overlay--visible");
-      success.classList += " modal__overlay--visible";
+      loading.classList.remove(`modal__overlay--visible`);
+      success.classList += ` modal__overlay--visible`;
     })
     .catch(() => {
-      loading.classList.remove("modal__overlay--visible");
+      loading.classList.remove(`modal__overlay--visible`);
       alert(
-        "The email service is temporarily unavailable. Please contact me directly on email@email.com"
+        `This email service is temporarily unavailable. Please contact me directly on eyesofthegoddess@hotmail.com`
       );
     });
 }
@@ -51,8 +51,8 @@ function contact(event) {
 function toggleModal() {
   if (isModalOpen) {
     isModalOpen = false;
-    return document.body.classList.remove("modal--open");
+    return document.body.classList.remove(`modal--open`);
   }
   isModalOpen = true;
-  document.body.classList += " modal--open";
+  document.body.classList += ` modal--open`;
 }
